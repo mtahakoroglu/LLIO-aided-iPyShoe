86a440f2e2164ef2757ab6239dd6ded3fcde0cf7
Taha Koroglu <m.tahakoroglu@gmail.com>
2024-08-25 03:10:56 +0300

    median filtering kernel size is now automatically computed to detect the number of strides taken by the pedestrian

--- plot_own_sensor_data.py
+++ plot_own_sensor_data.py
@@ -86,11 +86,7 @@ for file in sensor_data_files:
         # that yields incorrect count of strides taken by the pedestrian.
         if det_list[i] == 'lstm':
             logging.info(f"Plotting zero velocity detection for median filtered {det_list[i]} detector for file: {file}")
-            # print(f"Kernel size is {k[j]} for the current median filtering process.")
-            # zv_lstm_filtered = medfilt(zv, k[j])
-            # zv_lstm_filtered[:100] = 1 # make sure all labels are zero at the beginning as the foot is stationary
-            # n, strideIndex = count_zero_to_one_transitions(zv_lstm_filtered)
-            k = 5
+            k = 5 # median filter kernel size
             while True:
                 k = k+2
                 print(f"Kernel size is {k} for the current median filtering process.")
